12e2718cc876d4a0191a444c1d2ecc9744716c20
Klas Kalass <klas.r.kalass@googlemail.com>
2025-05-14 19:07:50 +0200

    Fixed test, which was wrongly adjusted to api change

--- test/src/real_files_test.dart
+++ test/src/real_files_test.dart
@@ -17,24 +17,23 @@ void main() {
       final foafTurtleFile = File('test/assets/foaf.ttl');
       final expectedTurtle = foafTurtleFile.readAsStringSync();
       final rdfCore = RdfCore.withStandardCodecs(
-        additionalCodecs: [
-          RdfXmlCodec(
-            encoderOptions: RdfXmlEncoderOptions.readable().copyWith(
-              customPrefixes: {
-                'wot': 'http://xmlns.com/wot/0.1/',
-                // override the default prefix for schema.org which goes to https://schema.org/
-                'schema': 'http://schema.org/',
-              },
-            ),
-          ),
-        ],
+        additionalCodecs: [RdfXmlCodec()],
       );
       final graph = rdfCore.decode(
         xmlContent,
         contentType: 'application/rdf+xml',
         documentUrl: 'http://xmlns.com/foaf/0.1/',
       );
-      final turtle = rdfCore.encode(graph);
+      final turtle = rdfCore.encode(
+        graph,
+        options: TurtleEncoderOptions(
+          customPrefixes: {
+            'wot': 'http://xmlns.com/wot/0.1/',
+            // override the default prefix for schema.org which goes to https://schema.org/
+            'schema': 'http://schema.org/',
+          },
+        ),
+      );
 
       _log.finest('Serialized FOAF to Turtle format: $turtle');
       expect(turtle.trim(), equals(expectedTurtle.trim()));
